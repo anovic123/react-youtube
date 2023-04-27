9af806c1dc76659090efec62789e23a3c4581b63
anovic123 <105565287+anovic123@users.noreply.github.com>
2023-04-27 19:06:45 +0300

    added load more for home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { v1 } from 'uuid';
 
 import { HomeVideo, Loader } from '../../components';
@@ -10,21 +10,54 @@ import { ContentVideo } from '../../common/types/video/Video';
 import s from './style.module.scss';
 
 export const HomePage: FC = () => {
-  const { data, error, loading } = useFetch(`video/related-contents/?id=kJQP7kiw5Fk&hl=en&gl=US`);
+  const [cursorToken, setCursorToken] = useState('');
+
+  const { data, error, loading } = useFetch(
+    `video/related-contents/?id=kJQP7kiw5Fk&hl=en&gl=US${
+      cursorToken ? `&cursorNext=${cursorToken}` : ''
+    }`,
+  );
+
+  const [contents, setContents] = useState<ContentVideo[]>([]);
+
+  const handleScroll = () => {
+    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+
+    if (scrollHeight - scrollTop === clientHeight) {
+      if (!data?.cursorNext || data?.cursorNext === cursorToken) {
+        return;
+      }
+
+      setCursorToken(data?.cursorNext);
+    }
+  };
 
   useEffect(() => {
     document.title = 'YouTube - Main Page';
 
+    window.addEventListener('scroll', handleScroll);
+
     return () => {
       document.title = 'YouTube';
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [data, cursorToken]);
+
+  useEffect(() => {
+    if (data?.contents) {
+      setContents((prevContents) => [...prevContents, ...data.contents]);
+    }
+  }, [data]);
 
   return (
     <>
       <div className={s.wrapper}>
         {error && <div>{error}</div>}
-        {loading ? <Loader /> : data?.contents?.map((v: ContentVideo) => <HomeVideo key={v1()} video={v} />)}
+        {loading ? (
+          <Loader />
+        ) : (
+          contents.map((v: ContentVideo) => <HomeVideo key={v1()} video={v} />)
+        )}
       </div>
     </>
   );
